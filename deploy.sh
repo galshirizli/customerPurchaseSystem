87bc36002bec1a8644b48cb2cebbe897256110d3
gal shirizli <gal.shirizly@gmail.com>
2024-05-24 14:10:50 +0300

    Try HPA - frontend and kafka

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NAMESPACE="customer-purchase-system"
+
+echo "------ Applying Namespace... ------"
+kubectl apply -f ./kubernetes-manifest/namespace.yaml
+echo "------ Namespace applied. ------"
+
+echo "------ Applying Zookeeper and MongoDB... ------"
+kubectl apply -f ./kubernetes-manifest/zookeeper.yaml
+kubectl apply -f ./kubernetes-manifest/mongo.yaml
+echo " ------ Zookeeper and MongoDB applied. Waiting for them to be available... ------"
+kubectl wait --for=condition=available --timeout=300s deployment/zookeeper -n $NAMESPACE &
+kubectl wait --for=condition=available --timeout=300s deployment/mongo -n $NAMESPACE &
+wait
+echo "------ Zookeeper and MongoDB are available. ------"
+
+echo "------ Applying Kafka... ------"
+kubectl apply -f ./kubernetes-manifest/kafka.yaml
+echo "------ Kafka applied. Waiting for Kafka to be available... ------"
+kubectl wait --for=condition=available --timeout=300s deployment/kafka -n $NAMESPACE
+echo "------ Kafka is available. ------"
+
+echo "------ Applying Backend and Frontend... ------"
+kubectl apply -f ./kubernetes-manifest/backend.yaml
+kubectl apply -f ./kubernetes-manifest/frontend.yaml
+echo "------ Backend and Frontend applied. Waiting for them to be available... ------"
+kubectl wait --for=condition=available --timeout=300s deployment/backend -n $NAMESPACE &
+kubectl wait --for=condition=available --timeout=300s deployment/frontend -n $NAMESPACE &
+wait
+echo "------ Backend and Frontend are available. ------"
+
+echo "------ All resources have been successfully applied and are available. ------"
